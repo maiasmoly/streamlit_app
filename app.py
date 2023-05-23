7dcf06e68df8182de17e57b1ac173e518874b9ec
Maia Smolyanov <maia.smolyanov@gmail.com>
2023-05-23 10:24:02 -0700

    Added a file uploader and summary statistics for the uploaded file

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
 
-agree = st.checkbox('I agree')
+uploaded_file = st.file_uploader("Choose a file to upload! Make sure it's a csv!")
 
-if agree:
-    st.write('Great!')
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+    st.write("Above is our data frame from the uploaded data! Here are some summary statistics:")
+    st.write("The size of our data frame is ", df.shape)
+    st.write(df.describe())
+
+
